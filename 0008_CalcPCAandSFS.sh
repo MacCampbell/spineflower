1d7dc5a2e6803f8194d2f96dc40e0764ba77d954
MacCampbell <campbellmatthewa@gmail.com>
2021-07-27 13:23:31 -0800

    Identified paralogs

---
+++ 0008_CalcPCAandSFS.sh
@@ -0,0 +1,9 @@
+
+
+
+
+##Using 0007/nonparalogous-contigs.tsv as a region file let's generate a PCA and a SFS for sanity
+
+
+## SFS example command
+./angsd -bam bam.filelist -doSaf 1 -out smallFolded -anc  chimpHg19.fa -GL 2 -minMapQ 10 -minQ 20
