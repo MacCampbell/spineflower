a94efab77216c20f66539cbacbdc4bd9566c2744
maccampbell <campbellmatthewa@gmail.com>
2021-09-08 13:43:00 -0800

    Changning delims in calc_pwfst

--- 0011_calc_pwfst.sh
+++ 0011_calc_pwfst.sh
@@ -23,7 +23,7 @@ echo "#!/bin/bash
 #SBATCH --job-name=fst${x}
 #SBATCH -n 8
 #SBATCH -N 1
-#SBATCH --partition=high
+#SBATCH --partition=med
 #SBATCH --time=48:00:00
 #SBATCH --output=0011/${pop1}_${pop2}-%j.slurmout
 #############################################
@@ -32,7 +32,7 @@ realSFS 0011/${pop1}.saf.idx 0011/${pop2}.saf.idx > 0011/${pop1}_${pop2}.2dsfs
 #Rscript scripts/plot2DSFS_2D.R 0011/${pop1}_${pop2}.2dsfs \$(wc -l 0011/${pop1}.bamlist | awk '{print \$1}') \$(wc -l 0011/${pop2}.bamlist | awk '{print \$1}') $pop1 $pop2
 #Rscript scripts/plot2dSFS.R 0011/${pop1}_${pop2}.2dsfs 0011/${pop1}_${pop2}.2dsfs.pdf ${pop1} ${pop2}
 realSFS fst index 0011/${pop1}.saf.idx 0011/${pop2}.saf.idx -sfs 0011/${pop1}_${pop2}.2dsfs -fstout 0011/${pop1}_${pop2} 
-realSFS fst stats 0011/${pop1}_${pop2}.fst.idx > 0011/${pop1}_${pop2}.fst.stats
+realSFS fst stats 0011/${pop1}_${pop2}.fst.idx > 0011/${pop1}_-_${pop2}.fst.stats
 " > fst_${pop1}_${pop2}.sh
 
 sbatch fst_${pop1}_${pop2}.sh
@@ -43,6 +43,6 @@ done
 
 
 #hold til complete
-grep "" 0011/*fst.stats | sed 's/.fst.stats:/      /' | sed 's:0011/::' |  tr "_" "\t" | awk '{print $1"_"$2"_"$3"\t"$4"_"$5"_"$6"\t"$8}' > 0011/pwfst.all
+grep "" 0011/*fst.stats | sed 's/.fst.stats:/      /' | sed 's:0011/::' |  tr "_-_" "\t" | awk '{print $1"_"$2"_"$3"\t"$4"_"$5"_"$6"\t"$8}' > 0011/pwfst.all
 
 1000_scripts/pwlist_to_matrix.pl 0011/pwfst.all > 0011/pwfst.fstmatrix
